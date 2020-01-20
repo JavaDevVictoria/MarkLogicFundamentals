cb6ba2ad31ba0ea1f33179b87f8ae6d3fc6e5ba2
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2020-01-20 10:22:36 +0000

    Create InsertJSONDocument2.js

---
+++ InsertJSONDocument2.js
@@ -0,0 +1,11 @@
+declareUpdate();
+xdmp.documentInsert('/patents/magic_translator.json', 
+{
+  "patent": 
+  {
+    "market": "spies", 
+    "title": "Magic listening device", 
+    "inventor": "Tamas", 
+    "info": "Small earpiece that translates foreign language conversations for the listener in real time"
+  }
+});
